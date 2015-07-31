d38303000a42d7bd35b29cbe2740e9f1b859515b
evanjmg <evanjmg@gmail.com>
2015-07-31 23:38:12 +0100

    setup devise, still working on oauth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,41 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+  def instagram
+    raise
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      raise
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
+    else
+      session["devise.instagram_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { sessions: "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks"}
+  resources :users, only: [:index]
  root 'users#index'
  
 end
