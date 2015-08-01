d70fd7385c9ae4f91d46554b8804562c879e79a7
evanjmg <evanjmg@gmail.com>
2015-08-01 21:13:38 +0100

    cleaned up files

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
- # def instagram
- #   @user = User.find_for_oauth(env["omniauth.auth"], current_user)
-
- #   if @user.persisted?
- #     # Sign in a user and tries to redirect first to the stored location 
- #     # Then to the url specified by after_sign_in_path_for.
- #     sign_in_and_redirect @user, event: :authentication
-
- #     # Set a flash message
- #     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
- #   else
- #     session["devise.#{provider}_data"] = env["omniauth.auth"]
- #     redirect_to new_user_registration_url
- #   end
- # end
+ 
  def self.provides_callback_for(provider)
      class_eval %Q{
        def #{provider}
@@ -47,37 +27,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
        finish_signup_path(resource)
      end
    end
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
 
-# def instagram
-
-#   @user = User.from_omniauth(request.env["omniauth.auth"])
-
-#   if @user.persisted?
-   
-#     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-#     set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
-#   else
-#     session["devise.instagram_data"] = request.env["omniauth.auth"]
-#     redirect_to new_user_registration_url
-#   end
-# end
