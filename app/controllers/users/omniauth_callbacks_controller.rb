2cf17d4a6e073d92fec72d1804e63e5e9fdf5436
evanjmg <evanjmg@gmail.com>
2015-08-01 17:44:55 +0100

    modifying oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,11 +6,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
   def instagram
-    raise
+ 
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      raise
+     
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { sessions: "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks"}
-  resources :users, only: [:index]
- root 'users#index'
+  get 'search/new'
+
+  get 'search/index'
+
+  devise_for :users, :skip => [:sessions], :controllers => {  :omniauth_callbacks => "users/omniauth_callbacks"}
+  # get 'users/index', to: "users#index", as: 'users' 
+  devise_scope :user do
  
+    authenticated :user do
+      root 'search#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'users/sessions#new', as: :unauthenticated_root
+    end
+  end
+
 end
