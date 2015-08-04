565cf8a63668f6b40e0a3dda79d87799a8abfd34
evanjmg <evanjmg@gmail.com>
2015-08-04 09:47:14 +0100

    got aws working

--- app/uploaders/image_file_uploader.rb
+++ app/uploaders/image_file_uploader.rb
@@ -4,10 +4,9 @@ class ImageFileUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  # include CarrierWave::MimeTypes process :set_content_type
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -32,11 +31,9 @@ class ImageFileUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [150, 150]
   end
-  version :thumb do
-    process :resize_to_fit => [300, 300]
-  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
